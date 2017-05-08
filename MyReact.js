b400ba8aca07236617909969e8a8c1748d712694
xuefei <xuefei@beisen.com>
2017-05-08 18:00:58 +0800

    完成，用简易todo实践

--- MyReact.js
+++ MyReact.js
@@ -43,6 +43,19 @@ var React = {
 	}
 }
 
+function _shouldUpdateReactComponent(prevElement, nextElement){
+	if(prevElement != null && nextElement != null){
+		var prevType = typeof PrevElement;
+		var nextType = typeof nextElement;
+		if(prevType === 'string' || prevType === 'number'){
+			return nextType === 'string' || nextType === 'number';
+		} else {
+			return nextType === 'object' && prevElement.type === nextElement.type && prevElement.key === nextElement.key;
+		}
+	}
+	return false;
+}
+
 function instantiateReactComponent(element) {
 	if (typeof element === 'string' | typeof element === 'number') {
 		return new ReactDOMTextComponent(element)
@@ -66,6 +79,9 @@ function ReactClass(){
 }
 
 ReactClass.prototype.render = function(){}
+ReactClass.prototype.setState = function(newState){
+	this._reactInternalInstance.receiveComponent(null,newState)
+}
 
 /**
  * 文字元素处理
@@ -80,6 +96,13 @@ ReactDOMTextComponent.prototype.mountComponent = function(rootId) {
 	return '<span data-reactid="' + rootId + '">' + this._currentElement + "</span>";
 }
 
+ReactDOMTextComponent.prototype.receiveComponent = function(nextText) {
+	var nextStringText = '' + nextText;
+	if(nextStringText !== this._currentElement){
+		this._currentElement = nextStringText;
+		$('[data-reactid="' + this._rootNodeId + '"]').html(this._currentElement)
+	}
+}
 
 /**
  * element为 用createElement处理之后返回的  ReactElement 的实例，具有 type/key/props
@@ -95,13 +118,13 @@ ReactDOMComponent.prototype.mountComponent = function(rootId){
 	var tagOpen = '<' + this._currentElement.type;
 	var tagClose = '</' + this._currentElement.type + '>';
 
-	tagOpen += ' data-reactid=' + this._rootNodeId;
+	tagOpen += ' data-reactid="' + this._rootNodeId +'"';
 
 	for(var propKey in props){
 		if(/^on[A-Z]/.test(propKey)){
 			//[改变  处理onClick事件]
-			var eventType = propKey.replace(/on([A-Z])/,function(all,one){return one.toLowerCase()});
-			$(document).delegate('[data-reactid="' + this._rootNodeId+'"]',eventType + '.' + this._rootNodeId,props[propKey] )
+			var eventType = propKey.replace(/on([A-Za-z]+)/,function(all,one){return one.toLowerCase()});
+			$(document).delegate('[data-reactid="' + this._rootNodeId+'"]',eventType + '.' + this._rootNodeId, props[propKey] )
 		}
 
 		if(props[propKey] && propKey != 'children' && !/^on[A-Z]/.test(propKey)){
@@ -114,6 +137,9 @@ ReactDOMComponent.prototype.mountComponent = function(rootId){
 
 	var childrenInstances = [];
 	var that = this;
+	if(that._rootNodeId===undefined){
+		debugger;
+	}
 	$.each(children,function(key,child){
 		//本质还是递归渲染所有子组件，并进行实例化
 
@@ -122,7 +148,11 @@ ReactDOMComponent.prototype.mountComponent = function(rootId){
 		childComponentInstance._mountIndex = key;
 
 		childrenInstances.push(childComponentInstance);
+		if(that._rootNodeId===undefined){
+			debugger;
+		}
 		var curRootId = that._rootNodeId + '.' + key;
+		console.log('所有的rootid： ',curRootId)
 		var childMarkup = childComponentInstance.mountComponent(curRootId);
 
 		content += ' ' + childMarkup;
@@ -132,6 +162,217 @@ ReactDOMComponent.prototype.mountComponent = function(rootId){
 
 	//拼接出所有内容
 	return tagOpen + '>' + content + tagClose;
+}
+
+ReactDOMComponent.prototype.receiveComponent = function(nextElement){
+	var lastProps = this._currentElement.props;
+	var nextProps = nextElement.props;
+
+	this._currentElement = nextElement;
+	this._updateDOMProperties(lastProps,nextProps);
+	this._updateDOMChildren(nextElement.props.children);
+}
+
+ReactDOMComponent.prototype._updateDOMProperties = function(lastProps, nextProps){
+	var propKey;
+	for(propKey in lastProps){
+		if(nextProps.hasOwnProperty(propKey) || !lastProps.hasOwnProperty(propKey)){
+			continue;
+		}
+		if(/^on[A-Z]/.test(propKey)){
+			var eventType = propKey.replace(/on([A-Za-z]+)/,function(all,one){return one.toLowerCase()});
+            $(document).undelegate('[data-reactid="' + this._rootNodeId + '"]', eventType, lastProps[propKey]);
+            continue;
+		}
+		$('[data-reactid="' + this._rootNodeId + '"]').removeAttr(propKey)
+	}
+
+	for(propkey in nextProps){
+		if(/^on[A-Z]/.test(propKey)){
+			var eventType = propKey.replace(/on([A-Za-z]+)/,function(all,one){return one.toLowerCase()});
+			lastProps[propKey] && $(document).undelegate('[data-reactid="' + this._rootNodeId + '"]', eventType, lastProps[propKey]);
+			$(document).delegate('[data-reactid="' + this._rootNodeId + '"]',eventType + '.' + this._rootNodeId, nextProps[propKey]);
+			continue;
+		}
+		if(propKey === 'children') continue;
+		$('[data-reactid="' + this._rootNodeId + '"]').prop(propKey, nextProps[propKey])
+	}
+
+}
+
+var updateDepth = 0;
+var diffQueue = [];
+ReactDOMComponent.prototype._updateDOMChildren = function(nextChildrenElements){
+	updateDepth++;
+	this._diff(diffQueue,nextChildrenElements);
+	updateDepth--;
+	if(updateDepth == 0){
+		this._patch(diffQueue);
+		diffQueue = [];
+	}
+}
+
+/**
+ * _diff和_patch逻辑
+ */
+var UPDATE_TYPES = {
+	MOVE_EXISTING:1,
+	REMOVE_NODE:2,
+	INSERT_MARKUP:3
+}
+
+function flattenChildren(componentChildren){
+	var child;
+	var name;
+	var childrenMap = {};
+	for(var i = 0; i < componentChildren.length; i++){
+		child = componentChildren[i];
+		name = child && child._currentElement && child._currentElement.key ? child._currentElement.key : i.toString(36);
+		childrenMap[name] = child;
+	} 
+	return childrenMap;
+}
+
+function generateComponentChildren(prevChildren, nextChildrenElements){
+	var nextChildren = {};
+	nextChildrenElements = nextChildrenElements || [];
+	$.each(nextChildrenElements, function(index, element){
+		//【bug 应该是element._currentElement.key吧？ 不是，这儿是this.props.children传进来的，为字符串，reactelement和reactclass三种最原始的可能】
+		//而 prevChildren 是_renderedChildren传进来的，是实例化之后的
+		var name = element.key ? element.key : index; 
+		var prevChild = prevChildren && prevChildren[name];
+		var prevElement = prevChild && prevChild._currentElement;
+		var nextElement = element;
+
+		if(_shouldUpdateReactComponent(prevElement, nextElement)){
+			prevChild.receiveComponent(nextElement);
+			nextChildren[name] = prevChild;
+		} else {
+			var nextChildInstance = instantiateReactComponent(nextElement,null)
+			nextChildren[name] = nextChildInstance;
+		}
+	})
+
+	return nextChildren;
+}
+
+ReactDOMComponent.prototype._diff = function(diffQueue, nextChildrenElements){
+	var self = this;
+	var prevChildren = flattenChildren(self._renderedChildren);
+	var nextChildren = generateComponentChildren(prevChildren, nextChildrenElements);
+	self._renderedChildren = []
+	$.each(nextChildren, function(key, instance){
+		self._renderedChildren.push(instance);
+	})
+
+	var lastIndex = 0;
+	var nextIndex = 0;
+
+	for(var name in nextChildren){
+		if(!nextChildren.hasOwnProperty(name)){
+			continue;
+		}
+		var prevChild = prevChildren && prevChildren[name];
+		var nextChild = nextChildren[name];
+
+		if(prevChild === nextChild){
+			prevChild._mountIndex < lastIndex && diffQueue.push({
+				parentId: self._rootNodeId,
+				parentNode: $('[data-reactid="' + self._rootNodeId + '"]'),
+				type: UPDATE_TYPES.MOVE_EXISTING,
+				fromIndex: prevChild._mountIndex,
+				toIndex: nextIndex
+			})
+			lastIndex = Math.max(prevChild._mountIndex, lastIndex);
+		} else {
+			if( prevChild ){
+				diffQueue.push({
+					type: UPDATE_TYPES.REMOVE_NODE,
+					parentId: self._rootNodeId,
+					parentNode:$('[data-reactid="' + self._rootNodeId + '"]'),
+					fromIndex: prevChild._mountIndex,
+					toIndex: null
+				})
+
+				if(prevChild._rootNodeId){
+					$(document).undelegate('.' + prevChild._rootNodeId);
+				}
+
+				lastIndex = Math.max(prevChild._mountIndex, lastIndex);
+			}
+
+			diffQueue.push({
+				parentId: self._rootNodeId,
+				parentNode: $('[data-reactid="' + self._rootNodeId + '"]'),
+				type:UPDATE_TYPES.INSERT_MARKUP,
+				fromIndex:null,
+				toIndex: nextIndex,
+				markup: nextChild.mountComponent(self._rootNodeId + '.' + name)
+			})
+		}
+
+		nextChild._mountIndex = nextIndex;
+		nextIndex++;
+	}
+
+	for(var name in prevChildren){
+		if(prevChildren.hasOwnProperty(name) && !(nextChildren && nextChildren.hasOwnProperty(name))){
+			diffQueue.push({
+				parentId: self._rootNodeId,
+				parentNode: $('[data-reactid="' + self._rootNodeId + '"]'),
+				type: UPDATE_TYPES.REMOVE_NODE,
+				fromIndex: prevChildren[name]._mountIndex,
+				toIndex: null
+			})
+
+			if(prevChildren[name]._rootNodeId){
+				$(document).undelegate('.' + prevChildren[name]._rootNodeId);
+			}
+		}
+	}
+}
+
+function insertChildAt(parentNode, childNode, index){
+	var beforeChild = parentNode.children().get(index);
+	beforeChild ? childNode.insertBefore(beforeChild) : childNode.appendTo(parentNode);
+}
+
+ReactDOMComponent.prototype._patch = function(updates){
+	var update;
+	var initialChildren = {};
+	var deleteChildren = [];
+	for(var i = 0; i < updates.length; i++){
+		update = updates[i];
+		if(update.type === UPDATE_TYPES.MOVE_EXISTING || update.type === UPDATE_TYPES.REMOVE_NODE){
+			var updatedIndex = update.fromIndex;
+			var updatedChild = $(update.parentNode.children().get(updatedIndex));
+			var parentId = update.parentId;
+
+			initialChildren[parentId] = initialChildren[parentId] || [];
+			initialChildren[parentId][updatedIndex] = updatedChild;
+
+
+			deleteChildren.push(updatedChild)
+		}
+	}
+
+	$.each(deleteChildren, function(index, child){
+		$(child).remove();
+	})
+
+	for(var k = 0; k < updates.length; k++){
+		update = updates[k];
+		switch(update.type){
+			case UPDATE_TYPES.INSERT_MARKUP:
+				insertChildAt(update.parentNode, $(update.markup), update.toIndex);
+				break;
+			case UPDATE_TYPES.MOVE_EXISTING:
+				insertChildAt(update.parentNode, initialChildren[update.parentId][update.fromIndex], update.toIndex);
+				break;
+			case UPDATE_TYPES.REMOVE_NODE:
+				break;
+		}
+	}
 }
 
 /**
@@ -172,6 +413,45 @@ ReactCompositeComponent.prototype.mountComponent = function(rootId){
 
 	return renderedMarkup;
 
+}
+
+ReactCompositeComponent.prototype.receiveComponent = function(nextElement,newState){
+	/**
+	 * 它主要做了什么事呢？首先会合并改动，生成最新的state,props然后拿以前的render返回的element跟现在最新调用
+	 * render生成的element进行对比（_shouldUpdateReactComponent），看看需不需要更新，
+	 * 如果要更新就继续调用对应的component类对应的receiveComponent
+	 */
+	 //【这儿是类元素没有改变的逻辑 如果改变，不会走这一个，是直接instate，然后mount】
+	this._currentElement = nextElement || this._currentElement; //用新的子节点更新子节点 都是createElement返回的内容，包括 ~.render()返回的
+
+	var inst = this._instance;
+
+	var nextState = $.extend(inst.state,newState);
+	var nextProps = this._currentElement.props;
+
+	if(inst.shouldComponentUpdate && (inst.shouldComponentUpdate(nextProps,nextSate) === false)) return;
+	if(inst.componentWillUpdate) inst.componentWillUpdate(nextProps,nextState);
+
+	inst.state = nextState;
+	inst.props = nextProps;
+
+	var prevComponentInstance = this._renderedComponent; //上次render返回的石磊，算是子节点了.
+	var prevRenderedElement = prevComponentInstance._currentElement;
+
+	var nextRenderedElement = this._instance.render();
+
+	if(_shouldUpdateReactComponent(prevRenderedElement,nextRenderedElement)){
+		prevComponentInstance.receiveComponent(nextRenderedElement)
+		inst.componentDidUpdate && inst.componentDidUpdate();
+	} else {
+		var thisId = this._rootNodeId;
+		var renderedComponent = instantiateReactComponent(nextRenderedElement);
+		var nextMarkup = renderedComponent.mountComponent(thisId)
+		this._renderedComponent = renderedComponent;
+
+		$('[data-reactid="' + this._rootNodeId + '"]').replaceWith(nextMarkup);
+
+	}
 }
 
 
